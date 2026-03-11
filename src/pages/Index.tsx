ed697e906c1ae14151dd4d2d64700050e2b5544c
Imeth Dewmina <imeth.20240076@iit.ac.lk>
2026-03-11 17:39:46 +0530

    disbale the liquid esther

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,26 +11,6 @@ const Index = () => {
   return (
     <ClickSpark sparkColor="#fff" sparkSize={10} sparkRadius={15} sparkCount={8} duration={400}>
       <div className="min-h-screen bg-background relative">
-        {/* Global LiquidEther background */}
-        <div className="fixed inset-0 z-0 pointer-events-none">
-          <LiquidEther
-            colors={['#5227FF', '#FF9FFC', '#B19EEF']}
-            mouseForce={20}
-            cursorSize={100}
-            isViscous
-            viscous={30}
-            iterationsViscous={32}
-            iterationsPoisson={32}
-            resolution={0.5}
-            isBounce={false}
-            autoDemo
-            autoSpeed={0.5}
-            autoIntensity={2.2}
-            takeoverDuration={0.25}
-            autoResumeDelay={3000}
-            autoRampDuration={0.6}
-          />
-        </div>
         <div className="relative z-10">
           <Navbar />
           <HeroSection />
